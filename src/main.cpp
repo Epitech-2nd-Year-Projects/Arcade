d51719d61724e6226e1c3af219c4dd562355815f
Yanis <yanis.kernoua@epitech.eu>
2025-03-21 09:19:54 +0100

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+static void usage(const std::string &name)
+{
+    std::cerr << "Usage: " << name << "<path_to_display_library>" << std::endl;
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2) {
+        usage(argv[0]);
+        return 84;
+    }
+
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
